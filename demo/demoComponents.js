eb45c917cbf002b500dabe33aaeb4a7564fdbb27
Matthew Browne <mbrowne83@gmail.com>
2019-08-30 14:59:53 -0400

    refactored to use IIFE instead of Object.assign() (previous approach was incorrect use of PURE annotations)

--- demo/demoComponents.js
+++ demo/demoComponents.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-// export function FunctionComponent() {
-//     return React.createElement('div')
-// }
-// FunctionComponent.displayName = 'Foo'
-// FunctionComponent.defaultProps = {}
+export function FunctionComponent() {
+    return React.createElement('div')
+}
+FunctionComponent.displayName = 'Foo'
+FunctionComponent.defaultProps = {}
 
 export class ClassComponent extends React.Component {
     static displayName = 'Bar'
